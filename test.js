716dd610885964a04b753bb0c152d89d215b048f
Titus Wormer <tituswormer@gmail.com>
2023-07-06 15:36:35 +0200

    Change to use `export` map

--- test.js
+++ test.js
@@ -5,13 +5,14 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
-import {headingStyle} from './index.js'
+import {headingStyle} from 'mdast-util-heading-style'
 
 test('headingStyle', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
-      'headingStyle'
-    ])
+    assert.deepEqual(
+      Object.keys(await import('mdast-util-heading-style')).sort(),
+      ['headingStyle']
+    )
   })
 
   await t.test('should fail without node', async function () {
